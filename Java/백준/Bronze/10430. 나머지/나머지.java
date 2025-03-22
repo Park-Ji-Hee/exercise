2b67ce6b1ed84e0b77c6ca351c4927bd1e05d283
Park-Ji-Hee <128693838+Park-Ji-Hee@users.noreply.github.com>
2025-03-22 20:19:07 +0900

    [Bronze V] Title: 나머지, Time: 180 ms, Memory: 17696 KB -BaekjoonHub

--- Java/백준/Bronze/10430. 나머지/나머지.java
+++ Java/백준/Bronze/10430. 나머지/나머지.java
@@ -1,21 +1,19 @@
-// (A+B)%C,  ((A%C) + (B%C))%C, (A×B)%C, ((A%C) × (B%C))%C
-
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        int num1 = scanner.nextInt();
-        int num2 = scanner.nextInt();
-        int num3 = scanner.nextInt();
-
-        System.out.println((num1 + num2) % num3);
-        System.out.println(((num1 % num3) + (num2 % num3)) % num3);
-        System.out.println((num1 * num2) % num3);
-        System.out.println(((num1 % num3) * (num2 % num3)) % num3);
+    	
+    	Scanner sc = new Scanner(System.in);
+    	
+    	int a = sc.nextInt();
+    	int b = sc.nextInt();
+    	int c = sc.nextInt();
+    	
+    	System.out.println((a+b)%c);
+    	System.out.println(((a%c) + (b%c))%c);
+    	System.out.println((a*b)%c);
+    	System.out.println(((a%c) * (b%c))%c);
+		
     }
 }
-
-
