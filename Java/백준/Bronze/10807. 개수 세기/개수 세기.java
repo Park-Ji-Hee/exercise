708e8c77f1fb2e6411a0042beabea73faaf9914d
Park-Ji-Hee <128693838+Park-Ji-Hee@users.noreply.github.com>
2025-03-28 08:34:58 +0900

    [Bronze V] Title: 개수 세기, Time: 180 ms, Memory: 17872 KB -BaekjoonHub

--- Java/백준/Bronze/10807. 개수 세기/개수 세기.java
+++ Java/백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -4,23 +4,24 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int t = sc.nextInt();  
-        int[] a = new int[t];  
-        int cnt = 0;
-
-
-        for (int i = 0; i < t; i++) {
-            a[i] = sc.nextInt();
+        int a = sc.nextInt();
+        
+        int []arr = new int[a];
+        
+        for(int i = 0 ; i <a; i++) {
+             arr[i] = sc.nextInt();   
         }
-
-        int b = sc.nextInt();
-        for (int i = 0; i < t; i++) {
-            if (a[i] == b) {
-                 cnt++;
+        
+        int b= sc.nextInt();
+        
+        int cvt = 0;
+        for(int i = 0 ; i <a; i++) {
+            if(arr[i] == b) {
+            	cvt++;
             }
-        }
-
-        System.out.println(cnt);
+       }
+        
+        System.out.println(cvt);
         sc.close();
     }
 }
