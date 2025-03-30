dd634fa110775452fd84ad2350273a3c21c3b141
Park-Ji-Hee <128693838+Park-Ji-Hee@users.noreply.github.com>
2025-03-30 11:12:24 +0900

    [level 0] Title: 편지, Time: 0.03 ms, Memory: 76.1 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/120898. 편지/편지.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(String message) {
+        int answer = 0;
+        int message_len = message.length();
+        
+        answer = message_len *2;
+        
+        return answer;
+    }
+}
