2cc526e15542e7411e9d6d5d2bdc31dc41c2d767
Park-Ji-Hee <128693838+Park-Ji-Hee@users.noreply.github.com>
2025-04-29 21:12:52 +0900

    [level 0] Title: 약수 구하기, Time: 0.68 ms, Memory: 84.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int n) {
+        List<Integer> list = new ArrayList<>();
+        
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                list.add(i); 
+            }
+        }
+        
+        int[] answer = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+    
+        Arrays.sort(answer);
+        
+        return answer;
+    }
+}
